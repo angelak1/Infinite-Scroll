c247d95e49e98e20bbb55cc85a9d596db0be51fa
Angela Kearns <angela.kearns@nike.com>
2021-08-19 10:52:16 -0700

    Initial Commit

---
+++ Shared/Infinite_ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  Infinite_ScrollApp.swift
+//  Shared
+//
+//  Created by Angela Kearns on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Infinite_ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
